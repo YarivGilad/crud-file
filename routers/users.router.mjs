8b7515a1e868ecef8371176fc78e68b4d8288572
Yariv Gilad <yariv.gilad@gmail.com>
2024-03-20 21:12:04 +0200

    step 2

--- routers/users.router.mjs
+++ routers/users.router.mjs
@@ -1,37 +1,67 @@
+import fs from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from "node:url";
 // import express from 'express';
 import { Router } from 'express';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // const router = express.Router();
 const router = Router();
 
+const DB_PATH = path.resolve(__dirname , '../data/users.json');
+const HTTP_LOG_PATH = path.resolve(__dirname , '../logs/http.log');
+
+async function getUsersFromFile(req,res,next){
+    try{
+        let users = await fs.readFile(DB_PATH,'utf-8');
+        users = JSON.parse(users);
+        req.users = users;
+        next();
+    }catch(err){
+        next(err);
+    }
+}
+
+async function logHTTP(req,res,next){
+     await fs.appendFile(
+        HTTP_LOG_PATH,
+        `${req.method} ${req.originalUrl} ${Date.now()}\n`
+        )
+    next();
+}
+
+router.use(logHTTP);
+router.use(getUsersFromFile);
 
 // GET All users
-router.get('/',(req,res)=> {
-    res.status(200).send('GET All users');
+router.get('/', async (req,res)=> {
+    res.status(200).json(req.users);
 });
+
 // GET user by id
-router.get('/:id',(req,res)=> {
-    res.status(200).send('GET user by id');
+router.get('/:id', async (req,res,next)=> {
+    const user = req.users.find(u => u.id === req.params.id);
+    res.status(200).json(user);
 });
 
 // Create new user
-router.post('/',(req,res)=> {
+router.post('/', async (req,res)=> {
     res.status(200).send('Create new user');
 })
 
 // Update (replace) user   /api/users/:id
-router.put('/:id',(req,res)=> {
+router.put('/:id', async (req,res)=> {
     res.status(200).send('Update (replace) user');
 })
 
 // Update (merge) user
-router.patch('/:id',(req,res)=> {
+router.patch('/:id',async (req,res)=> {
     res.status(200).send('Update (merge) user');
 })
 
 // DELETE user
-router.delete('/:id',(req,res)=> {
+router.delete('/:id', async (req,res)=> {
     res.status(200).send('DELETE user');
 })
 
--- server.mjs
+++ server.mjs
@@ -1,3 +1,7 @@
+// built in Node.js modules
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from "node:url";
+import fs from 'node:fs/promises';
 // installed npm packages
 import express from 'express';
 import morgan from 'morgan';
@@ -5,7 +9,10 @@ import log from '@ajar/marker';
 // local project modules
 import usersRouter from './routers/users.router.mjs';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const { HOST, PORT } = process.env;
+const ERRORS_LOG_PATH = path.resolve(__dirname , './logs/errors.log');
 
 //creating the express app
 const app = express();
@@ -18,8 +25,16 @@ app.use(express.json());
 app.use('/api/users',usersRouter);
 
 // error handling
-app.use((req,res)=>{
+app.use((req,res)=> {
     res.status(404).send(`${req.method}: ${req.url} not found...`);
+})
+
+app.use(async (err,req,res,next)=> {
+    await fs.appendFile(
+        ERRORS_LOG_PATH,
+        `${req.method} ${req.originalUrl} ${Date.now()}\n\t${err.message}\n`
+        )
+    res.status(500).send('Something went wrong... Internal server error...')
 })
 
 // start the app
